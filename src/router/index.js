bdf6acd1f91215d337563016345dea3febb5a1d0
TheCreeep <thecreepfire@gmail.com>
2023-05-26 19:48:55 +0200

    Moved riot.txt to public

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+/* import { createRouter, createWebHistory } from 'vue-router'
+import 'riot.txt' from '../riot.txt'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/riot.txt',
+      name: 'riot',
+      component: () => import('../riot.txt')
+    }
+  ]
+})
+
+export default router */
